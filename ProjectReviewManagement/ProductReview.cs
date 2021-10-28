ee0aa1847b97606c190509a37a07fd46ab1dbb8b
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-28 19:25:45 +0530

    [Chaitanya] Add : Added Review model and created list of reviews

--- ProjectReviewManagement/ProductReview.cs
+++ ProjectReviewManagement/ProductReview.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace ProjectReviewManagement
 {
-    class ProductReview
+    public class ProductReview
     {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
     }
 }
--- ProjectReviewManagement/Program.cs
+++ ProjectReviewManagement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProjectReviewManagement
 {
@@ -6,7 +7,23 @@ namespace ProjectReviewManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IList<ProductReview> productReviews = new List<ProductReview>() { };
+            List<string> reviewList = new List<string>() { "Good", "Bad", "Ok" };
+            Random random = new Random();
+            for (int i = 1; i <= 25; i++)
+            {
+                ProductReview review = new ProductReview();
+                review.ProductID = i;
+                review.UserID = random.Next(1, 11);
+                review.Rating = random.Next(1, 6);
+                review.Review = reviewList[random.Next(reviewList.Count)];
+                review.isLike = Convert.ToBoolean(random.Next(2));
+                productReviews.Add(review);
+            }
+            foreach(ProductReview review in productReviews)
+            {
+                Console.WriteLine($"{review.ProductID}-{review.UserID}-{review.Rating}-{review.Review}-{review.isLike}");
+            }
         }
     }
 }
